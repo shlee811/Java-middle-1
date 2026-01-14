1c035fd898eca4dd8a5706d5cd3daa15ed3c908d
shlee72729799 <2tjdgns811@naver.com>
2026-01-15 00:15:48 +0900

    섹션5 완료

---
+++ src/lang/math/test/LottoGeneratorMain.java
@@ -0,0 +1,23 @@
+package lang.math.test;
+
+public class LottoGeneratorMain {
+    public static void main(String[] args) {
+        LottoGenerator generator = new LottoGenerator();
+        int[] lottoNumbers = generator.generate();
+
+        // 생성된 로또 번호 출력
+        System.out.print("로또 번호: ");
+        for (int lottoNumber : lottoNumbers) {
+            System.out.print(lottoNumber + " ");
+        }
+    }
+}
+
+
+//문제 - 로또 번호 자동 생성기
+
+//**문제 설명**
+//로또 번호를 자동으로 만들어주는 자동 생성기를 만들자
+//로또 번호는 1~45 사이의 숫자를 6개 뽑아야 한다.
+//각 숫자는 중복되면 안된다.
+//실행할 때 마다 결과가 달라야 한다.
---
+++ src/lang/wrapper/test/WrapperTest2.java
@@ -0,0 +1,16 @@
+package lang.wrapper.test;
+
+public class WrapperTest2 {
+    public static void main(String[] args) {
+        String[] array = {"1.5", "2.5", "3.0"};
+        double sum = 0;
+        for (String s : array) {
+            double i = Double.parseDouble(s);
+            sum += i;
+        }
+        System.out.println("sum = " + sum);
+    }
+}
+
+
+//배열에 입력된 모든 숫자의 합을 구하자. 숫자는 `double` 형이 입력될 수 있다.
